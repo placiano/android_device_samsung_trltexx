7ee3cabc8cb65be99e07e6a8dc03a9f5f18ad417
placiano <placiano80@gmail.com>
2016-06-01 00:34:05 +0200

    trltexx: cmify's
    Signed-off-by: placiano <placiano80@gmail.com>

--- cm.mk
+++ cm.mk
@@ -17,10 +17,10 @@
 $(call inherit-product, device/samsung/trltexx/full_trltexx.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/emotion/configs/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit common EMOTION phone.
-$(call inherit-product, vendor/emotion/configs/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := trltexx
-PRODUCT_NAME := emotion_trltexx
+PRODUCT_NAME := cm_trltexx
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo emotion_trltexx-userdebug
+add_lunch_combo cm_trltexx-userdebug
 
